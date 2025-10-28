8f91a5c724bb3c90508cf23e906573de948e616a
Marius Niveri <marius.niveri@gmail.com>
2025-10-28 23:46:18 +0100

    nvim/leap: migrate to new version that removed add_default_mappings (use its old def)

--- modules/home-manager/modules/neovim/nvim/lua/plugins/leap.lua
+++ modules/home-manager/modules/neovim/nvim/lua/plugins/leap.lua
@@ -4,7 +4,18 @@ return {
   name = "leap",
   event = "VeryLazy",
   init = function()
-    require("leap").add_default_mappings()
+    for _, _22_ in ipairs({ { { "n", "x", "o" }, "s", "<Plug>(leap-forward)", "Leap forward" }, { { "n", "x", "o" }, "S", "<Plug>(leap-backward)", "Leap backward" }, { { "x", "o" }, "x", "<Plug>(leap-forward-till)", "Leap forward till" }, { { "x", "o" }, "X", "<Plug>(leap-backward-till)", "Leap backward till" }, { { "n", "x", "o" }, "gs", "<Plug>(leap-from-window)", "Leap from window" } }) do
+      local modes = _22_[1]
+      local lhs = _22_[2]
+      local rhs = _22_[3]
+      local desc = _22_[4]
+      for _0, mode in ipairs(modes) do
+        if ((vim.fn.mapcheck(lhs, mode) == "") and (vim.fn.hasmapto(rhs, mode) == 0)) then
+          vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+        else
+        end
+      end
+    end
     vim.keymap.set({ "n", "o" }, "gS", function()
       require("leap.remote").action()
     end)
