8cb042a5e919359e290bbd508b0f11f74f164984
Marius Niveri <marius.niveri@gmail.com>
2025-10-28 23:45:32 +0100

    nvim/avante: migrate to new version using acp

--- modules/home-manager/modules/neovim/nvim/lua/plugins/avante.lua
+++ modules/home-manager/modules/neovim/nvim/lua/plugins/avante.lua
@@ -1,83 +1,63 @@
 return {
-  "m4r1vs/avante.nvim",
+  "yetone/avante.nvim",
   version = false,
+  event = "VeryLazy",
   keys = {
     { "<leader>aa", "<cmd>AvanteToggle<cr>", mode = "n" },
     { "<leader>aa", "<cmd>AvanteAsk<cr>",    mode = "v" },
     { "<leader>ae", "<cmd>AvanteEdit<cr>",   mode = "v" },
   },
   opts = {
-    provider = "gemini",
-    mappings = {
-      ask = "<leader>tt"
-    },
-    gemini = {
-      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-      model = "gemini-2.5-pro-preview-05-06",
-      timeout = 30000,
-      temperature = 0,
-      api_key_name = "cmd:secret-tool lookup google_gemini password",
-      max_tokens = 40960,
-    },
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o",
-      timeout = 30000,
-      temperature = 0,
-      api_key_name = "cmd:secret-tool lookup openai password",
-      --reasoning_effort = "medium",
-    },
-    rag_service = {
-      enabled = false,
-      host_mount = os.getenv("HOME"),
-      runner = "nix",
-      provider = "openai",
-      llm_model = "",
-      embed_model = "",
-      endpoint = "https://api.openai.com/v1",
-      api_key_name = "cmd:secret-tool lookup openai password",
-      docker_extra_args = "",
-    },
-    behaviour = {
-      enable_token_counting = false,
-      support_paste_from_clipboard = false,
-      auto_suggestions = false,
-    },
-    hints = {
-      enabled = false,
+    -- add any opts here
+    -- this file can contain specific instructions for your project
+    instructions_file = "avante.md",
+    -- for example
+    provider = "gemini-cli",
+    acp_providers = {
+      ["gemini-cli"] = {
+        command = "gemini",
+        args = { "--experimental-acp" },
+        env = {
+          NODE_NO_WARNINGS = "1",
+        },
+      },
     },
   },
-  build = "make",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    "echasnovski/mini.pick",
-    "nvim-telescope/telescope.nvim",
-    "hrsh7th/nvim-cmp",
-    "ibhagwan/fzf-lua",
-    "nvim-tree/nvim-web-devicons",
+    --- The below dependencies are optional,
+    "nvim-mini/mini.pick",           -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+    "stevearc/dressing.nvim",        -- for input provider dressing
+    "folke/snacks.nvim",             -- for input provider snacks
+    "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
     {
+      -- support for image pasting
       "HakonHarnes/img-clip.nvim",
       event = "VeryLazy",
       opts = {
+        -- recommended settings
         default = {
           embed_image_as_base64 = false,
           prompt_for_file_name = false,
           drag_and_drop = {
             insert_mode = true,
           },
+          -- required for Windows users
           use_absolute_path = true,
         },
       },
     },
     {
+      -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
       opts = {
-        file_types = { "Avante" },
+        file_types = { "markdown", "Avante" },
       },
-      ft = { "Avante" },
+      ft = { "markdown", "Avante" },
     },
   },
 }
