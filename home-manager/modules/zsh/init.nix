80f760c1a5360e8d69072fbdb8f10363ffac96c9
Marius Niveri <marius.niveri@gmail.com>
2025-11-10 11:03:08 +0100

    zsh: force english language (esp. for git)

--- home-manager/modules/zsh/init.nix
+++ home-manager/modules/zsh/init.nix
@@ -1,6 +1,7 @@
 pkgs:
 # bash
 ''
+  export LANGUAGE=en_US.UTF-8
   export FZF_DEFAULT_OPTS="--bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview-window=right,65%"
   export _ZO_FZF_OPTS="--tmux 80%,80% --bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview \"FILE=\"{}\"; FILE=\"\$(echo \"\$FILE\" | sed \"s/\'//g\")\"; ${pkgs.lsd}/bin/lsd --git --icon=always --color=always --tree --depth=2 /\"\''${FILE#*/}\"\""
   zvm_after_init_commands+=(eval "$(${pkgs.fzf}/bin/fzf --zsh)")
