8f3770a109fbd56a6f676b47033a26e4c36a811c
Marius Niveri <marius.niveri@gmail.com>
2025-11-14 01:14:27 +0100

    zsh: add LANG env var

--- home-manager/modules/zsh/init.nix
+++ home-manager/modules/zsh/init.nix
@@ -1,7 +1,10 @@
 pkgs:
 # bash
 ''
+  export LANG=en_US.UTF-8
   export LANGUAGE=en_US.UTF-8
+  export NIXPKGS_ALLOW_UNFREE=1
+
   export FZF_DEFAULT_OPTS="--bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview-window=right,65%"
   export _ZO_FZF_OPTS="--tmux 80%,80% --bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview \"FILE=\"{}\"; FILE=\"\$(echo \"\$FILE\" | sed \"s/\'//g\")\"; ${pkgs.lsd}/bin/lsd --git --icon=always --color=always --tree --depth=2 /\"\''${FILE#*/}\"\""
   zvm_after_init_commands+=(eval "$(${pkgs.fzf}/bin/fzf --zsh)")
@@ -14,6 +17,4 @@ pkgs:
   }
   zle -N zoxide_jump
   bindkey -M 'viins' '^Z' zoxide_jump
-
-  export NIXPKGS_ALLOW_UNFREE=1
 ''
