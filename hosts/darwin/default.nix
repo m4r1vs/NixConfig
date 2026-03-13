a9e856adebdce4c1c8921110328d005b46c4e345
Marius Niveri <marius.niveri@gmail.com>
2026-03-13 01:30:09 +0100

    darwin: nixify more options

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -59,26 +59,54 @@ in {
       };
     };
 
+    power = {
+      restartAfterFreeze = true;
+      sleep = {
+        allowSleepByPowerButton = true;
+        computer = 15;
+        display = 10;
+      };
+    };
+
     system = {
       configurationRevision = self.rev or self.dirtyRev or null;
       stateVersion = 6;
       primaryUser = systemArgs.username;
       defaults = {
         LaunchServices.LSQuarantine = false;
+        WindowManager = {
+          AppWindowGroupingBehavior = true;
+          EnableStandardClickToShowDesktop = false;
+          EnableTilingByEdgeDrag = false;
+          EnableTilingOptionAccelerator = false;
+          EnableTopTilingByEdgeDrag = false;
+          GloballyEnabled = false;
+          StandardHideDesktopIcons = true;
+          StandardHideWidgets = false;
+        };
         dock = {
+          appswitcher-all-displays = true;
           autohide = true;
           autohide-delay = 0.0;
-          static-only = false;
           expose-group-apps = false;
-          largesize = 36;
+          largesize = 48;
           launchanim = true;
           magnification = true;
           mineffect = "genie";
+          minimize-to-application = true;
           mru-spaces = false;
           orientation = "bottom";
-          minimize-to-application = true;
           show-recents = false;
-          tilesize = 28;
+          showAppExposeGestureEnabled = false;
+          showDesktopGestureEnabled = false;
+          showLaunchpadGestureEnabled = false;
+          showMissionControlGestureEnabled = true;
+          static-only = false;
+          tilesize = 38;
+          wvous-bl-corner = 1; # Hot corner (bottom left) -- disable
+          wvous-br-corner = 1; # Hot corner (bottom right) -- disable
+          wvous-tl-corner = 1; # Hot corner (top left) -- disable
+          wvous-tr-corner = 1; # Hot corner (top right) -- disable
         };
         trackpad = {
           Clicking = false;
@@ -89,6 +117,7 @@ in {
         finder = {
           _FXShowPosixPathInTitle = true; # Show full POSIX path
           _FXSortFoldersFirst = true; # Show folders first when sorting
+          _FXEnableColumnAutoSizing = true; # Resize columns to fit filenames
           AppleShowAllExtensions = true; # Show file extensions
           AppleShowAllFiles = true; # Show hidden files by default
           CreateDesktop = true; # When no window is focus, focus Finder (if false, yabai breaks a bit)
@@ -96,9 +125,34 @@ in {
           FXEnableExtensionChangeWarning = false; # Do not warn when changing file extension
           FXPreferredViewStyle = "Nlsv"; # Default to list view
           FXRemoveOldTrashItems = true; # Auto remove trash after 30 days
+          ShowPathbar = true; # Show path breadcrumbs in finder windows
+          ShowStatusBar = true; # Show status bar at bottom of finder windows with item/disk space stats
         };
         NSGlobalDomain = {
+          _HIHideMenuBar = false;
+          "com.apple.keyboard.fnState" = true;
           "com.apple.mouse.tapBehavior" = null;
+          "com.apple.swipescrolldirection" = true;
+          KeyRepeat = 2;
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = true;
+          NSAutomaticInlinePredictionEnabled = true;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = true;
+          NSAutomaticSpellingCorrectionEnabled = false;
+          NSAutomaticWindowAnimationsEnabled = true;
+          NSDocumentSaveNewDocumentsToCloud = false;
+          NSNavPanelExpandedStateForSaveMode = true;
+          NSNavPanelExpandedStateForSaveMode2 = true;
+          NSScrollAnimationEnabled = true;
+          NSStatusItemSelectionPadding = 2;
+          NSStatusItemSpacing = 6;
+          NSTableViewDefaultSizeMode = 1;
+          NSUseAnimatedFocusRing = true;
+          NSWindowResizeTime = 0.1;
+          NSWindowShouldDragOnGesture = true;
+          PMPrintingExpandedStateForPrint = true;
+          PMPrintingExpandedStateForPrint2 = true;
         };
         universalaccess.closeViewScrollWheelToggle = true; # Scroll while holding ctrl to zoom
         screencapture = {
@@ -110,6 +164,7 @@ in {
         SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
         controlcenter = {
           AirDrop = false;
+          BatteryShowPercentage = false;
           Bluetooth = true;
           Display = false;
           FocusModes = false;
@@ -118,8 +173,9 @@ in {
         };
         CustomUserPreferences = {
           "com.apple.dock" = {
-            "workspaces-auto-swoosh" = true;
+            "workspaces-auto-swoosh" = true; # Force automatic space switching when cmd-tabbing
           };
+          # Disable press-and-hold action in these apps
           "md.obsidian" = {
             "ApplePressAndHoldEnabled" = false;
           };
