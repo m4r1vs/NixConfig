20fdb8b1f65fa6f0281cb233a11e5c138d45d1af
Marius Niveri <marius.niveri@gmail.com>
2026-03-23 00:12:43 +0100

    virtnix: do not luks encrypt

--- hosts/virtnix/disks.nix
+++ hosts/virtnix/disks.nix
@@ -8,49 +8,21 @@
           type = "gpt";
           partitions = {
             ESP = {
-              label = "boot";
-              name = "ESP";
               size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [
-                  "umask=0077"
-                ];
+                mountOptions = ["umask=0077"];
               };
             };
-            luks = {
+            root = {
               size = "100%";
-              label = "luks";
               content = {
-                type = "luks";
-                name = "cryptroot";
-                passwordFile = "/tmp/secret.key";
-                settings.allowDiscards = true;
-                content = {
-                  type = "btrfs";
-                  extraArgs = ["-L" "nixos" "-f"];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/swap" = {
-                      mountpoint = "/swap";
-                      swap.swapfile.size = "10G";
-                    };
-                  };
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
