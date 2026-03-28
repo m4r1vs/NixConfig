3ae5078c747e991390519f2d04e86cf0c3598d8a
Marius Niveri <marius.niveri@gmail.com>
2026-03-28 19:49:53 +0100

    home: add descriptions to every package

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -13,70 +13,69 @@
 in {
   home.packages = with pkgs;
     [
-      astroterm
-      fastfetch
-      kubectl
-      golazo
-      pastel
-      xdg-utils
+      astroterm # show stars in terminal
+      fastfetch # new neofetch
+      kubectl # kubernetes CLI
+      golazo # show soccer scores in terminal
+      pastel # manipulate colors and palettes
+      xdg-utils # xdg-open, etc.
     ]
     ++ lib.optionals isDarwin [
-      clippy-darwin
-      comma
+      clippy-darwin # cli to copy/paste files (used by yazi plugin)
+      comma # run programs not installed but in nixpkgs
       (writeShellScriptBin "random-album-of-the-day" scripts.random-album-of-the-day)
     ]
     ++ lib.optionals isGraphical [
-      android-tools
-      atai
-      dbeaver-bin
-      gemini-cli
-      obsidian
-      prismlauncher
-      yt-dlp
-      zathura
+      android-tools # android studio and emulator
+      atai # openai wrapper to write in the terminal for me
+      dbeaver-bin # database explorer (postgres, mysql, etc.)
+      gemini-cli # vibecoding
+      obsidian # notes
+      prismlauncher # minecraft mod launcher
+      yt-dlp # youtube downloader
+      zathura # pdf viewer
     ]
     ++ lib.optionals isDesktop ([
-        amberol
-        diebahn
-        gimp-with-plugins
-        gnome-chess
-        gnome-clocks
-        gnome-decoder
-        gnome-network-displays
-        inkscape-with-extensions
-        jetbrains.idea
-        kdePackages.kwalletmanager
-        libnotify
-        nautilus
-        networkmanagerapplet
-        pavucontrol
-        polkit_gnome
-        scripts.artkube
-        stockfish
-        wireplumber
+        amberol # fancy mp3 player
+        diebahn # deutsche bahn arrivals/departures/delays
+        gimp-with-plugins # maxxed out gimp
+        gnome-chess # chess
+        gnome-clocks # alarms, timers, etc.
+        gnome-decoder # qr code generator
+        gnome-network-displays # airplay, chromecast, miracast
+        inkscape-with-extensions # maxxed out inkscape
+        jetbrains.idea # intellij idea ultimate
+        kdePackages.kwalletmanager # view keychain
+        libnotify # send notifications from terminal
+        nautilus # file browser
+        networkmanagerapplet # show wifi/ethernet in sys. tray
+        pavucontrol # sound manager
+        polkit_gnome # policy agent
+        stockfish # chess engine to play against computer
+        wireplumber # pipewire manager
       ]
       ++ (
         if isWayland
         then [
-          hyprcursor
-          hyprpicker
-          hyprshot
-          hyprutils
-          swayimg
+          hyprcursor # fancy cursor
+          hyprpicker # color picker
+          hyprshot # screenshot tool
+          hyprutils # hyprland stuff
+          swayimg # image viewer
         ]
         else [
-          arandr
-          feh
+          arandr # x11 monitor manager (resolution, position, etc.)
+          feh # set wallpaper on x11
         ]
       )
       ++ (
         if isX86
         then [
-          discord
-          spotify
+          discord # discord
+          spotify # spotify
         ]
         else [
-          legcord
+          legcord # discord alternative
         ]
       ));
 }
