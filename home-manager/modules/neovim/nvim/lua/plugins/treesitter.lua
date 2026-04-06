1b6eec2b4d3da714fee1c6ac83f206ef5fb7b285
Marius Niveri <marius.niveri@gmail.com>
2026-04-06 15:40:13 +0200

    nvim/treesitter: fix again

--- home-manager/modules/neovim/nvim/lua/plugins/treesitter.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/treesitter.lua
@@ -1,52 +1,52 @@
-local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
-vim.fn.mkdir(parser_install_dir, "p")
-vim.opt.runtimepath:append(parser_install_dir)
-
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false,
-    build = ":TSUpdate",
-    event = "VeryLazy",
-    branch = "master",
-    opts = {
-      parser_install_dir = parser_install_dir,
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      ensure_installed = {
-        "arduino",
-        "c",
-        "cpp",
-        "css",
-        "dart",
-        "dot",
-        "go",
-        "html",
-        "javascript",
-        "json",
-        "julia",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "nix",
-        "python",
-        "query",
-        "regex",
-        "ruby",
-        "rust",
-        "scss",
-        "sql",
-        "svelte",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-        "zig",
-      },
-    },
-  },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  opts = {},
+  config = function()
+    local parsers = {
+      "arduino",
+      "bash",
+      "c",
+      "cpp",
+      "css",
+      "dart",
+      "dot",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "julia",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "nix",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "rust",
+      "scss",
+      "sql",
+      "svelte",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+      "zig",
+    }
+    require("nvim-treesitter").setup {}
+    require("nvim-treesitter").install(parsers)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
+      end,
+    })
+  end,
 }
