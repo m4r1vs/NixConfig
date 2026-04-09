bc97532b0310ba07425f2a1f394add0e264b45c5
Marius Niveri <marius.niveri@gmail.com>
2026-04-09 15:22:42 +0200

    nvim/mini-ai: remap keybinds to free an/in from neovim 0.12

--- home-manager/modules/neovim/nvim/lua/plugins/mini.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/mini.lua
@@ -4,7 +4,12 @@ return {
     -- Enable aliases for common surrounding chars (for example c-i-q to change inside any quotes)
     "echasnovski/mini.ai",
     version = false,
-    opts = {}
+    opts = {
+      mappings = {
+        around_next = 'al',
+        inside_next = 'il',
+      }
+    }
   },
   {
     -- Highlight the word under the cursor in the entire buffer
