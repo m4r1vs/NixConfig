c00dd52ed46dd9de018a21ba53078c5d108d8b02
Marius Niveri <marius.niveri@gmail.com>
2026-04-15 17:05:32 +0200

    system: replace lanzaboote with limine

--- nixos-modules/secureboot.nix
+++ nixos-modules/secureboot.nix
@@ -1,39 +1,61 @@
 {
   lib,
   config,
-  options,
+  systemArgs,
   ...
 }:
 with lib; let
   cfg = config.boot.configured.secureboot;
 in {
   options.boot.configured.secureboot = {
-    enable = mkEnableOption "Enable secureboot using Lanzaboot.";
-    consoleMode = mkOption {
-      type = types.int;
-      default = 2;
-      description = "The systemd-boot console mode to use.";
-    };
+    enable = mkEnableOption "Enable secureboot using Limine.";
     configLimit = mkOption {
       type = types.int;
-      default = 20;
-      description = "How many systemd-boot entries to show.";
+      default = 5;
+      description = "How many boot entries to show.";
+    };
+    windowsPartUUID = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "The PARTUUID of the Windows EFI partition.";
     };
   };
 
-  # Only evaluate if lanzaboote is imported
-  config = optionalAttrs (options.boot ? lanzaboote) (mkIf cfg.enable {
+  config = mkIf cfg.enable {
     boot = {
-      loader.systemd-boot.enable = mkForce false;
-      lanzaboote = {
-        enable = true;
-        pkiBundle = "/var/lib/sbctl";
-        configurationLimit = cfg.configLimit;
-        settings = {
-          console-mode = cfg.consoleMode;
-          timeout = 10;
+      loader = {
+        systemd-boot.enable = mkForce false;
+        limine = {
+          enable = true;
+          efiSupport = true;
+          secureBoot.enable = true;
+          enrollConfig = true;
+          maxGenerations = cfg.configLimit;
+          extraConfig = ''
+            TIMEOUT=10
+          '';
+          style = {
+            wallpapers = [../home-manager/wallpaper/Artemis_II_Earthset.jpg];
+            wallpaperStyle = "stretched";
+            interface = {
+              branding = "Moin, ${systemArgs.username}@${systemArgs.hostname} :)";
+              brandingColor = 2;
+              helpHidden = true;
+            };
+            graphicalTerminal = {
+              background = "BB15130F";
+              foreground = "e6e5df";
+              palette = "100f0f;d14d41;879a39;d0a215;4385be;ce5d97;3aa99f;878580";
+              brightPalette = "575653;af3029;66800b;ad8301;205ea6;a02f6f;24837b;e6e5df";
+              margin = 0;
+              font = {
+                scale = "2x2";
+                spacing = 1;
+              };
+            };
+          };
         };
       };
     };
-  });
+  };
 }
