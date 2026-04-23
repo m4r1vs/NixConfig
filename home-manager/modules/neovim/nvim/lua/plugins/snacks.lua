042ed6fe8aa78d016c2dc3e1af0383471e3f0fd0
Marius Niveri <marius.niveri@gmail.com>
2026-04-23 22:46:30 +0200

    nvim: add folke's snacks.image plugin

---
+++ home-manager/modules/neovim/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+-- Small QOL Plugins by Folke
+return {
+  "folke/snacks.nvim",
+  opts = {
+    image = { enabled = true },
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    picker = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+  }
+}
