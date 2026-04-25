14b484f415441c8d3d2c3b2b1f45f9f003a1ae3d
Marius Niveri <marius.niveri@gmail.com>
2026-04-25 15:09:46 +0200

    scripts/gracefull-systemctl: add better notification

--- home-manager/scripts/gracefull-systemctl.nix
+++ home-manager/scripts/gracefull-systemctl.nix
@@ -8,11 +8,27 @@
   gracefull-systemctl =
     pkgs.writeShellScript "gracefull-systemctl"
     ''
-      ${scripts.nixos-notify} -e "Gracefully Performing Action:" "$1"
-      ${pkgs.psmisc}/bin/killall -q -w brave
+
+      if (( hour >= 5 && hour < 12 )); then
+        GOODBYE_STRING="Goodbye  "
+      elif (( hour >= 13 && hour < 16 )); then
+        GOODBYE_STRING="Enjoy your Day  "
+      elif (( hour >= 16 && hour < 21 )); then
+        GOODBYE_STRING="Enjoy your Evening 󰖚 "
+      elif (( hour >= 21 || hour < 5 )); then
+        GOODBYE_STRING="Goodnight, Nightowl 󰖔 "
+      else
+        GOODBYE_STRING="Goodbye, Time-traveller  "
+      fi
+
+      ${scripts.nixos-notify} -e -t 10000 -h string:synchronous:startup-script "$GOODBYE_STRING" "I'm gracefully closing everything..."
+
       ${lib.optionalString (config.configured.system-sounds.enable && config.configured.system-sounds.shutdown.enable) ''
         ${pkgs.mpv}/bin/mpv --no-video --no-config --volume=80 --no-terminal ${config.configured.system-sounds.shutdown.soundFile}
       ''}
+
+      ${pkgs.psmisc}/bin/killall -q -w brave
+
       systemctl $1
     '';
 }
