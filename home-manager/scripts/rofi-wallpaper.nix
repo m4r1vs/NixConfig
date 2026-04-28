9fe9e5b7b64b664f4bad659c3825d9178c1c1957
Marius Niveri <marius.niveri@gmail.com>
2026-04-28 17:50:04 +0200

    scripts/rofi-wallpaper: change wording

--- home-manager/scripts/rofi-wallpaper.nix
+++ home-manager/scripts/rofi-wallpaper.nix
@@ -40,7 +40,7 @@
     CHOICE="''${FILES[$CHOICE_INDEX]}"
     WALLPAPER="$WALLPAPER_DIR/$CHOICE"
     if [ -f "$WALLPAPER" ]; then
-        ${scripts.nixos-notify} -e -i "$WALLPAPER" -h string:synchronous:wallpaper-change -t 1800 "Now showing" "$CHOICE"
+        ${scripts.nixos-notify} -e -i "$WALLPAPER" -h string:synchronous:wallpaper-change -t 1800 "New Wallpaper:" "$CHOICE"
         ${pkgs.swww}/bin/swww img "$WALLPAPER" --transition-type any --transition-fps 60 --transition-step 20 --transition-duration 1
     fi
   '';
