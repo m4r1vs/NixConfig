feb4dc520f01a0cde97873e91892f5aefe49b282
Marius Niveri <marius.niveri@gmail.com>
2026-04-28 21:04:35 +0200

    scripts/rebuild: ring the bell on failure instead of showing success notification

--- home-manager/scripts/rebuild.nix
+++ home-manager/scripts/rebuild.nix
@@ -25,7 +25,12 @@ in {
             echo -e "Rebuilding and switching to \e[32mdefault specialisation\e[0m."
             sudo nixos-rebuild switch --flake ~/NixConfig/#${systemArgs.hostname}
           fi
-          ${scripts.nixos-notify} -e -h string:synchronous:rebuild "System has been rebuilt" "and I've applied the latest configuration"
+
+          if [ $? -eq 0 ]; then
+            ${scripts.nixos-notify} -e -h string:synchronous:rebuild "System has been rebuilt" "and I've applied the latest configuration"
+          else
+            echo -e "\a"
+          fi
         ''
     );
 }
