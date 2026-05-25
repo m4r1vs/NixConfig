0dc960f3d70649ac6d359acd3ce535202676c69f
Marius Niveri <marius.niveri@gmail.com>
2026-05-25 13:10:08 +0200

    scripts/launch-once: add toggle option

--- home-manager/scripts/launch-once.nix
+++ home-manager/scripts/launch-once.nix
@@ -3,6 +3,7 @@
     command,
     grep,
     useHypr ? false,
+    toggle ? false,
   }:
     pkgs.writeShellScript "launch-once"
     (
@@ -11,8 +12,28 @@
         /*
         bash
         */
+        if toggle
+        then ''
+          CLIENT=$(${pkgs.hyprland}/bin/hyprctl clients -j | ${pkgs.jq}/bin/jq -r ".[] | select(.initialClass == \"${grep}\" or .initialTitle == \"${grep}\" or .class == \"${grep}\")")
+          if [ -n "$CLIENT" ]; then
+            ADDR=$(echo "$CLIENT" | ${pkgs.jq}/bin/jq -r ".address")
+            WORKSPACE=$(echo "$CLIENT" | ${pkgs.jq}/bin/jq -r ".workspace.id")
+            CURRENT_WORKSPACE=$(${pkgs.hyprland}/bin/hyprctl monitors -j | ${pkgs.jq}/bin/jq -r ".[] | select(.focused == true) | .activeWorkspace.id")
+
+            if [ "$WORKSPACE" = "$CURRENT_WORKSPACE" ]; then
+              ${pkgs.hyprland}/bin/hyprctl dispatch movetoworkspacesilent "special:minimized,address:$ADDR"
+            else
+              ${pkgs.hyprland}/bin/hyprctl dispatch movetoworkspace "$CURRENT_WORKSPACE,address:$ADDR"
+              ${pkgs.hyprland}/bin/hyprctl dispatch focuswindow address:$ADDR
+            fi
+            exit 0
+          else
+            ${command} &
+          fi
         ''
-          if ${pkgs.hyprland}/bin/hyprctl clients | grep -q "${grep}" > /dev/null; then
+        else ''
+          CLIENT=$(${pkgs.hyprland}/bin/hyprctl clients -j | ${pkgs.jq}/bin/jq -r ".[] | select(.initialClass == \"${grep}\" or .initialTitle == \"${grep}\" or .class == \"${grep}\")")
+          if [ -n "$CLIENT" ]; then
             exit 0
           else
             ${command} &
