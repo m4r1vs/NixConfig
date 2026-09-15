331796886e96b848dc00e2d6df49c1c745da9777
Marius Niveri <marius@meetovo.de>
2026-09-15 12:20:38 +0200

    zsh: inherit fzf default opts in fzf-tab

--- home-manager/modules/zsh/init.nix
+++ home-manager/modules/zsh/init.nix
@@ -18,6 +18,9 @@
   export FZF_DEFAULT_OPTS="--bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview-window=right,65%"
   export _ZO_FZF_OPTS="--tmux 80%,80% --bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview \"FILE=\"{}\"; FILE=\"\$(echo \"\$FILE\" | sed \"s/\'//g\")\"; ${pkgs.lsd}/bin/lsd --git --icon=always --color=always --tree --depth=2 /\"\''${FILE#*/}\"\""
   zvm_after_init_commands+=(eval "$(${pkgs.fzf}/bin/fzf --zsh)")
+
+  zstyle ':fzf-tab:*' use-fzf-default-opts yes
+
   zoxide_jump() {
     if zi; then
       zle push-line
